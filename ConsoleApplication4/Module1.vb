fdd4a6d7a7dd94637f59acc61de58417651f04b1
ss512824 <z0970017119@gmail.com>
2015-10-19 22:28:46 +0800

    www

---
+++ ConsoleApplication4/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("hello:")
+
+        Dim abby As Integer = Val(Console.ReadLine())
+        Dim a As Integer = 0
+        Dim numberarray(abby) As Integer
+
+        Do
+            numberarray(a) = Val(Console.ReadLine)
+            a = a + 1
+
+        Loop While (a < abby)
+
+
+
+
+    End Sub
+
+End Module
+
